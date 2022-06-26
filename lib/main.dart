228765073b35491dcf733c98511f66c37af1c2f8
RichardAntolis <c14170093@john.petra.ac.id>
2022-06-26 16:16:48 +0800

    Login And Register Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future <void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: FirebaseOptions(
+  apiKey: 'AIzaSyBDX7dcRB_0vh230eixygnBi2JSto-z7M4', 
+  appId: '1:813636585639:ios:bd1f2a24efedd525d43382', 
+  messagingSenderId: '813636585639', 
+  projectId: 'mycoffee-f5173'));
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.brown,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -16,8 +16,11 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = new TextEditingController();
   final TextEditingController passwordController = new TextEditingController();
 
+  String? errorMessage;
+  
   //Firebase
   final _auth = FirebaseAuth.instance;
+  
 
   @override
   Widget build(BuildContext context) {
@@ -61,7 +64,7 @@ class _LoginScreenState extends State<LoginScreen> {
             return ("Password is required for login");
           }
           if (!regex.hasMatch(value)) {
-            return ("Enter Valid Password (Minimun 6 Characters)");
+            return ("Enter Valid Password (Minimum 6 Characters)");
           }
       },
       onSaved: (value) {
@@ -163,6 +166,8 @@ class _LoginScreenState extends State<LoginScreen> {
   void signIn(String email, String password) async{
     if (_formKey.currentState!.validate())
     {
+      try
+      {
       await _auth.
         signInWithEmailAndPassword(email: email, password: password).then((uid) => {
         Fluttertoast.showToast(msg: "Login Successful!"),
@@ -170,6 +175,34 @@ class _LoginScreenState extends State<LoginScreen> {
       }).catchError((e){
         Fluttertoast.showToast(msg: e!.message);
       });
+    }
+    on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
     }
   }
 
